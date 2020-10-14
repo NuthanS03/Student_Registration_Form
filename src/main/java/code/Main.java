1b274e91fee570ab2797271ed4f3a2664fc1bce5
NuthanS03 <71179312+NuthanS03@users.noreply.github.com>
2020-10-14 12:40:22 +0530

    Update Main.java

--- src/main/java/code/Main.java
+++ src/main/java/code/Main.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
  
 @Controller
-public class MainController {
+public class Main{
      
     @GetMapping("/register")
     public String showForm(Model model) {
